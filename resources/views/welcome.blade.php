5dcf7aa7dd4afd7713a749b2d09b84d427af54bd
Josue Diaz <jdiaz77711@gmail.com>
2024-05-15 16:56:12 +0100

    End of day two of last project on laravel 30 days: Frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,13 @@
 <x-layout>
     <div class="space-y-10">
-        <section>
+        <section class="text-center pt-6">
+            <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
+            <form action="">
+                <input type="text" placeholder="Web Developer..." class="rounded-xl bg-white/25 border-white/10 px-5 py-4 w-full max-w-xl mt-6">
+            </form>
+        </section>
+
+        <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8">
                 <x-job-card></x-job-card>
@@ -11,7 +18,7 @@
 
         <section>
             <x-section-heading>Tags</x-section-heading>
-            <div class="mt-6 space-x-1">
+            <div class="mt-6 space-x-4">
                 <x-tag>Tag</x-tag>
                 <x-tag>Tag</x-tag>
                 <x-tag>Tag</x-tag>
@@ -27,6 +34,12 @@
 
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
+
+            <div class="mt-6 space-y-6">
+                <x-job-card-wide></x-job-card-wide>
+                <x-job-card-wide></x-job-card-wide>
+                <x-job-card-wide></x-job-card-wide>
+            </div>
         </section>
     </div>
 </x-layout>
